13982f99fb20132032f26f72ce334db0b22a9a0f
GregCatalano <greg@it-all.com>
2017-04-22 13:46:33 -0400

    update sample config

--- config/env-sample.php
+++ config/env-sample.php
@@ -1,26 +1,24 @@
 <?php
-// installation instructions: copy this to env.php and update as your environment dictates
-// note if env set to 'live', debug info will never be displayed even if debug set true
+
 return [
-    'env' => 'dev',
-    'debug' => true,
-    'database' => [
-        'name' => 'btqcm',
-        'username' => 'btqcm',
-        'password' => 'woolsocks',
-        'host' => '127.0.0.1'
-    ],
-    'phpmailer' => [
-        'protocol' => 'smtp',
-        'smtpHost' => '?.?.com',
-        'smtpPort' => 2525
-        // todo may need u/p vars here
-    ],
-    'errors' => [
-        'reportMethods' => ['echo', 'log', 'email'] // see ErrorHandler for rules
-    ],
+    'isLive' => false,
+
     'dirs' => [
         'admin' => 'private'
     ],
-    'sendNotificationEmails' => true, // set false on dev only to stop receiving emails. forced true on live site.
+
+    'database' => [
+        'name' => 'dbname',
+        'username' => 'dbuser',
+        'password' => 'dbpw',
+        'host' => '127.0.0.1',
+        'port' => 5432
+    ],
+
+    'phpmailer' => [
+        'protocol' => 'smtp',
+        'smtpHost' => 'smtphost.com',
+        'smtpPort' => 2525
+    ]
+
 ];
