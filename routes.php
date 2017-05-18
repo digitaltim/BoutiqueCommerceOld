a951e49ceb1ce9d26b36f376d9128732a4f47945
Tim McHugh <tim@digitaltim.com>
2017-05-18 09:44:41 -0400

    Updated line formatting

--- routes.php
+++ routes.php
@@ -12,39 +12,73 @@ use \Psr\Http\Message\ResponseInterface as Response;
 /////////////////////////////////////////
 // Routes that anyone can access
 
-$slim->get('/', 'It_All\BoutiqueCommerce\Controllers\HomeController:index')->setName('home');
+$slim->get('/',
+    'It_All\BoutiqueCommerce\Controllers\HomeController:index')
+    ->setName('home');
 /////////////////////////////////////////
 
 /////////////////////////////////////////
 // Routes that only non-authenticated users (Guests) can access
 
-$slim->get('/' . $config['dirs']['admin'], 'It_All\BoutiqueCommerce\UI\Views\AuthenticationView:getSignIn')->add(new GuestMiddleware($container))->setName('auth.signin');
+$slim->get('/' . $config['dirs']['admin'],
+    'It_All\BoutiqueCommerce\UI\Views\AuthenticationView:getSignIn')
+    ->add(new GuestMiddleware($container))
+    ->setName('auth.signin');
 
-$slim->post('/' . $config['dirs']['admin'], 'It_All\BoutiqueCommerce\Controllers\AuthController:postSignIn')->add(new GuestMiddleware($container));
+$slim->post('/' . $config['dirs']['admin'],
+    'It_All\BoutiqueCommerce\Controllers\AuthController:postSignIn')
+    ->add(new GuestMiddleware($container));
 
-$slim->get('/' . $config['dirs']['admin'] . '/signup', 'It_All\BoutiqueCommerce\UI\Views\AuthenticationView:getSignUp')->add(new GuestMiddleware($container))->setName('auth.signup');
+$slim->get('/' . $config['dirs']['admin'] . '/signup',
+    'It_All\BoutiqueCommerce\UI\Views\AuthenticationView:getSignUp')
+    ->add(new GuestMiddleware($container))
+    ->setName('auth.signup');
 
-$slim->post('/' . $config['dirs']['admin'] . '/signup', 'It_All\BoutiqueCommerce\Controllers\AuthController:postSignUp')->add(new GuestMiddleware($container));
+$slim->post('/' . $config['dirs']['admin'] . '/signup',
+    'It_All\BoutiqueCommerce\Controllers\AuthController:postSignUp')
+    ->add(new GuestMiddleware($container));
 
 /////////////////////////////////////////
 
 /////////////////////////////////////////
 // Routes that only authenticated users access
-$slim->get('/' . $config['dirs']['admin'] . '/signout', 'It_All\BoutiqueCommerce\UI\Views\AuthenticationView:getSignOut')->setName('auth.signout');
+$slim->get('/' . $config['dirs']['admin'] . '/signout',
+    'It_All\BoutiqueCommerce\UI\Views\AuthenticationView:getSignOut')
+    ->setName('auth.signout');
 
 // CRUD
-$slim->get('/CRUD/{table}', 'It_All\BoutiqueCommerce\UI\Views\Admin\CRUD\CrudView:index')->add(new AuthMiddleware($container))->setName('crud.show');
+$slim->get('/CRUD/{table}',
+    'It_All\BoutiqueCommerce\UI\Views\Admin\CRUD\CrudView:index')
+    ->add(new AuthMiddleware($container))
+    ->setName('crud.show');
 
-$slim->get('/CRUD/{table}/insert', 'It_All\BoutiqueCommerce\UI\Views\Admin\CRUD\CrudView:getInsert')->add(new AuthMiddleware($container))->setName('crud.getInsert');
+$slim->get('/CRUD/{table}/insert',
+    'It_All\BoutiqueCommerce\UI\Views\Admin\CRUD\CrudView:getInsert')
+    ->add(new AuthMiddleware($container))
+    ->setName('crud.getInsert');
 
-$slim->post('/CRUD/{table}/insert', 'It_All\BoutiqueCommerce\Controllers\CrudController:postInsert')->add(new AuthMiddleware($container))->setName('crud.postInsert');
+$slim->post('/CRUD/{table}/insert',
+    'It_All\BoutiqueCommerce\Controllers\CrudController:postInsert')
+    ->add(new AuthMiddleware($container))
+    ->setName('crud.postInsert');
 
-$slim->get('/CRUD/{table}/{primaryKey}', 'It_All\BoutiqueCommerce\UI\Views\Admin\CRUD\CrudView:getUpdate')->add(new AuthMiddleware($container))->setName('crud.getUpdate');
+$slim->get('/CRUD/{table}/{primaryKey}',
+    'It_All\BoutiqueCommerce\UI\Views\Admin\CRUD\CrudView:getUpdate')
+    ->add(new AuthMiddleware($container))
+    ->setName('crud.getUpdate');
 
-$slim->post('/CRUD/{table}/{primaryKey}', 'It_All\BoutiqueCommerce\Controllers\CrudController:postUpdate')->add(new AuthMiddleware($container))->setName('crud.postUpdate');
+$slim->post('/CRUD/{table}/{primaryKey}',
+    'It_All\BoutiqueCommerce\Controllers\CrudController:postUpdate')
+    ->add(new AuthMiddleware($container))
+    ->setName('crud.postUpdate');
 
-$slim->get('/CRUD/{table}/delete/{primaryKey}', 'It_All\BoutiqueCommerce\Controllers\CrudController:delete')->add(new AuthMiddleware($container))->setName('crud.delete');
+$slim->get('/CRUD/{table}/delete/{primaryKey}',
+    'It_All\BoutiqueCommerce\Controllers\CrudController:delete')
+    ->add(new AuthMiddleware($container))
+    ->setName('crud.delete');
 /////////////////////////////////////////
 
 // MVC Test
-$slim->get('/{table}', 'It_All\BoutiqueCommerce\UI\Views\ListView:output')->setName('table.show');
+$slim->get('/{table}',
+    'It_All\BoutiqueCommerce\UI\Views\ListView:output')
+    ->setName('table.show');
