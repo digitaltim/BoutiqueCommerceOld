8d9133b85ac11a0087423626963b21891c9f7490
GregCatalano <greg@it-all.com>
2017-06-13 07:15:19 -0400

    refactor views to move common code to parent

--- Src/Domain/Admin/Admins/AdminsView.php
+++ Src/Domain/Admin/Admins/AdminsView.php
@@ -10,27 +10,7 @@ class AdminsView extends AdminView
 {
     public function index($request, $response, $args)
     {
-        $res = (new AdminsModel)->select('id, username, name, role');
-
-        $insertLink = ($this->authorization->check($this->container->settings['authorization']['admins.insert'])) ? ['text' => 'Insert Admin', 'route' => 'admins.insert'] : false;
-
-        return $this->view->render(
-            $response,
-            'admin/list.twig',
-            [
-                'title' => 'Admins',
-                'primaryKeyColumn' => 'id',
-                'insertLink' => $insertLink,
-                'updateColumn' => 'username',
-                'updatePermitted' => $this->authorization
-                    ->check($this->container->settings['authorization']['admins.update']),
-                'updateRoute' => 'admins.put.update',
-                'addDeleteColumn' => true,
-                'deleteRoute' => 'admins.delete',
-                'table' => pg_fetch_all($res),
-                'navigationItems' => $this->navigationItems
-            ]
-        );
+        $this->indexView($response, new AdminsModel, 'admins', 'id, username, name, role');
     }
 
     private function setPersistPasswords(array &$fields): array
@@ -42,6 +22,13 @@ class AdminsView extends AdminView
         return $fields;
     }
 
+    /**
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     * note cannot call parent insertView because of persist passwords call
+     */
     public function getInsert($request, $response, $args)
     {
         $fields = (new AdminsModel)->getFormFields();
@@ -63,44 +50,6 @@ class AdminsView extends AdminView
 
     public function getUpdate($request, $response, $args)
     {
-        $adminsModel = new AdminsModel();
-        $id = intval($args['primaryKey']);
-
-        // make sure there is a record for the model
-        if (!$record = $adminsModel->selectForPrimaryKey($id)) {
-            $_SESSION['adminNotice'] = [
-                "Record $id Not Found",
-                'adminNoticeFailure'
-            ];
-            return $response->withRedirect($this->router->pathFor('admins.index'));
-        }
-
-        $fields = (new AdminsModel)->getFormFields('update');
-        $fields = $this->setPersistPasswords($fields);
-
-        /**
-         * data to send to FormHelper - either from the model or from prior input. Note that when sending null FormHelper defaults to using $_SESSION['formInput']. It's important to send null, not $_SESSION['formInput'], because FormHelper unsets $_SESSION['formInput'] after using it.
-         * note, this works for post/put because controller calls this method directly in case of errors instead of redirecting
-         */
-        if ($request->isGet()) {
-            $fieldData = $adminsModel->selectForPrimaryKey($id);
-            $fieldData['password_hash'] = ''; // do not display
-        } else {
-            $fieldData = null;
-        }
-
-        return $this->view->render(
-            $response,
-            'admin/form.twig',
-            [
-                'title' => 'Update Admin',
-                'formActionRoute' => 'admins.put.update',
-                'primaryKey' => $args['primaryKey'],
-                'formFields' => FormHelper::insertValuesErrors($fields, $fieldData),
-                'focusField' => FormHelper::getFocusField(),
-                'generalFormError' => FormHelper::getGeneralFormError(),
-                'navigationItems' => $this->navigationItems
-            ]
-        );
+        return $this->updateView($request, $response, $args, new AdminsModel(), 'admins');
     }
 }
--- Src/Domain/Admin/Marketing/Testimonials/TestimonialsView.php
+++ Src/Domain/Admin/Marketing/Testimonials/TestimonialsView.php
@@ -4,84 +4,22 @@ declare(strict_types=1);
 namespace It_All\BoutiqueCommerce\Src\Domain\Admin\Marketing\Testimonials;
 
 use It_All\BoutiqueCommerce\Src\Infrastructure\AdminView;
-use It_All\BoutiqueCommerce\Src\Infrastructure\UserInterface\FormHelper;
 
 class TestimonialsView extends AdminView
 {
+
     public function index($request, $response, $args)
     {
-        $res = (new TestimonialsModel)->select('id, enter_date, person, text, place, status');
-
-        return $this->view->render(
-            $response,
-            'admin/list.twig',
-            [
-                'title' => 'Testimonials',
-                'primaryKeyColumn' => 'id',
-                'insertLink' => ['text' => 'Insert Testimonial', 'route' => 'testimonials.insert'],
-                'updatePermitted' => $this->authorization
-                    ->check($this->container->settings['authorization']['testimonials.update']),
-                'updateRoute' => 'testimonials.put.update',
-                'addDeleteColumn' => true,
-                'deleteRoute' => 'testimonials.delete',
-                'table' => pg_fetch_all($res),
-                'navigationItems' => $this->navigationItems
-            ]
-        );
+        $this->indexView($response, new TestimonialsModel, 'testimonials');
     }
 
     public function getInsert($request, $response, $args)
     {
-        $fields = (new TestimonialsModel())->getFormFields('insert');
-
-        return $this->view->render(
-            $response,
-            'admin/form.twig',
-            [
-                'title' => 'Insert Testimonial',
-                'formActionRoute' => 'testimonials.post.insert',
-                'formFields' => FormHelper::insertValuesErrors($fields),
-                'focusField' => FormHelper::getFocusField(),
-                'generalFormError' => FormHelper::getGeneralFormError(),
-                'navigationItems' => $this->navigationItems
-            ]
-        );
+        return $this->insertView($response, new TestimonialsModel(), 'testimonials');
     }
 
     public function getUpdate($request, $response, $args)
     {
-        $testimonialsModel = new TestimonialsModel();
-        $id = intval($args['primaryKey']);
-
-        // make sure there is a record for the model
-        if (!$record = $testimonialsModel->selectForPrimaryKey($id)) {
-            $_SESSION['adminNotice'] = [
-                "Record $id Not Found",
-                'adminNoticeFailure'
-            ];
-            return $response->withRedirect($this->router->pathFor('testimonials.index'));
-        }
-
-        $fields = $testimonialsModel->getFormFields('update');
-
-        /**
-         * data to send to FormHelper - either from the model or from prior input. Note that when sending null FormHelper defaults to using $_SESSION['formInput']. It's important to send null, not $_SESSION['formInput'], because FormHelper unsets $_SESSION['formInput'] after using it.
-         * note, this works for post/put because controller calls this method directly in case of errors instead of redirecting
-         */
-        $fieldData = ($request->isGet()) ? $testimonialsModel->selectForPrimaryKey($id) : null;
-
-        return $this->view->render(
-            $response,
-            'admin/form.twig',
-            [
-                'title' => 'Update Testimonial',
-                'formActionRoute' => 'testimonials.put.update',
-                'primaryKey' => $args['primaryKey'],
-                'formFields' => FormHelper::insertValuesErrors($fields, $fieldData),
-                'focusField' => FormHelper::getFocusField(),
-                'generalFormError' => FormHelper::getGeneralFormError(),
-                'navigationItems' => $this->navigationItems
-            ]
-        );
+        return $this->updateView($request, $response, $args, new TestimonialsModel(), 'testimonials');
     }
 }
