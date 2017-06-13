1f302f869ea7310b1b994cbd490a3f7e2b3f181d
GregCatalano <greg@it-all.com>
2017-06-13 16:32:24 -0400

    comment routercachefile as it was causing problems

--- Src/config/slim3/settings.php
+++ Src/config/slim3/settings.php
@@ -9,7 +9,7 @@ return [
 
         'addContentLengthHeader' => false, // if this is not disabled, slim/App.php line 585 triggered an exception related to error handling, when the php set_error_handler() function was triggered
 
-        'routerCacheFile' => $config['storage']['pathCache'].'router.txt',
+//        'routerCacheFile' => $config['storage']['pathCache'].'router.txt',
 
         'authentication' => $config['maxFailedLogins'],
 
