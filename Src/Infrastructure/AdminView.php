bdcaf17eb78f13b6625383f9e2b2df0f29405887
GregCatalano <greg@it-all.com>
2017-06-13 11:41:54 -0400

    use model property

--- Src/Infrastructure/AdminView.php
+++ Src/Infrastructure/AdminView.php
@@ -30,31 +30,31 @@ class AdminView
 
     public function index($request, $response, $args)
     {
-        $this->indexView($response, $this->model, $this->routePrefix);
+        $this->indexView($response, $this->routePrefix);
     }
 
     public function getInsert($request, $response, $args)
     {
-        return $this->insertView($response, $this->model, $this->routePrefix);
+        return $this->insertView($response, $this->routePrefix);
     }
 
     public function getUpdate($request, $response, $args)
     {
-        return $this->updateView($request, $response, $args, $this->model, $this->routePrefix);
+        return $this->updateView($request, $response, $args, $this->routePrefix);
     }
 
-    protected function indexView($response, Model $model, string $routePrefix, string $columns = '*')
+    protected function indexView($response, string $routePrefix, string $columns = '*')
     {
-        $res = $model->select($columns);
+        $res = $this->model->select($columns);
 
-        $insertLink = ($this->authorization->check($this->container->settings['authorization'][$routePrefix.'.insert'])) ? ['text' => 'Insert '.$model->getFormalTableName(false), 'route' => $routePrefix.'.insert'] : false;
+        $insertLink = ($this->authorization->check($this->container->settings['authorization'][$routePrefix.'.insert'])) ? ['text' => 'Insert '.$this->model->getFormalTableName(false), 'route' => $routePrefix.'.insert'] : false;
 
         return $this->view->render(
             $response,
             'admin/list.twig',
             [
-                'title' => $model->getFormalTableName(),
-                'primaryKeyColumn' => $model->getPrimaryKeyColumnName(),
+                'title' => $this->model->getFormalTableName(),
+                'primaryKeyColumn' => $this->model->getPrimaryKeyColumnName(),
                 'insertLink' => $insertLink,
                 'updatePermitted' => $this->authorization
                     ->check($this->container->settings['authorization'][$routePrefix.'.update']),
@@ -67,15 +67,15 @@ class AdminView
         );
     }
 
-    protected function insertView($response, Model $model, string $routePrefix)
+    protected function insertView($response, string $routePrefix)
     {
-        $fields = $model->getFormFields('insert');
+        $fields = $this->model->getFormFields('insert');
 
         return $this->view->render(
             $response,
             'admin/form.twig',
             [
-                'title' => 'Insert '.$model->getFormalTableName(false),
+                'title' => 'Insert '.$this->model->getFormalTableName(false),
                 'formActionRoute' => $routePrefix.'.post.insert',
                 'formFields' => FormHelper::insertValuesErrors($fields),
                 'focusField' => FormHelper::getFocusField(),
@@ -85,18 +85,18 @@ class AdminView
         );
     }
 
-    protected function updateView($request, $response, $args, Model $model, string $routePrefix)
+    protected function updateView($request, $response, $args, string $routePrefix)
     {
         // make sure there is a record for the model
-        if (!$record = $model->selectForPrimaryKey($args['primaryKey'])) {
+        if (!$record = $this->model->selectForPrimaryKey($args['primaryKey'])) {
             $_SESSION['adminNotice'] = [
-                "Record $primaryKey Not Found",
+                "Record ".$args['primaryKey']." Not Found",
                 'adminNoticeFailure'
             ];
             return $response->withRedirect($this->router->pathFor($routePrefix.'.index'));
         }
 
-        $fields = $model->getFormFields('update');
+        $fields = $this->model->getFormFields('update');
 
         /**
          * data to send to FormHelper - either from the model or from prior input. Note that when sending null FormHelper defaults to using $_SESSION['formInput']. It's important to send null, not $_SESSION['formInput'], because FormHelper unsets $_SESSION['formInput'] after using it.
@@ -108,7 +108,7 @@ class AdminView
             $response,
             'admin/form.twig',
             [
-                'title' => 'Update ' . ucwords($model->getTableName()),
+                'title' => 'Update ' . ucwords($this->model->getTableName()),
                 'formActionRoute' => $routePrefix.'.put.update',
                 'primaryKey' => $args['primaryKey'],
                 'formFields' => FormHelper::insertValuesErrors($fields, $fieldData),
